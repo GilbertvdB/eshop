6b989fcc58981f475c47e034114b3e83eeb14df1
gb <gvanderbiezen@gmail.com>
2022-12-24 02:03:34 +0100

    created product model -mcrp

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -10,7 +10,7 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
                     {{ __("You're logged in!") }}
-                    Hello World! From Local Second test
+                    Products page
                 </div>
             </div>
         </div>
