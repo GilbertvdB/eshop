db0f9cab1c0ac40c334113edd12b22e47409c455
gb <gvanderbiezen@gmail.com>
2022-12-26 13:12:00 +0100

    Added wishlist service provider

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -10,7 +10,7 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
                     {{ __("You're logged in!") }}
-                    <br>Products page Helo
+                    <br>Products page
                     <br><a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('product.index') }}">
                             {{ __('Products Catalog') }}
                         </a>
