5d528bbd21d50205ed04aad10730a02f6910be6b
gb <gvanderbiezen@gmail.com>
2023-02-05 13:01:04 +0100

    bug - admin logout
    fixed admin logout issue. Changed route from get to post.
    logincontroller - middelware in contruct added..

--- routes/admin.php
+++ routes/admin.php
@@ -6,7 +6,7 @@ Route::group(['prefix'  =>  'admin'], function () {
 
 Route::get('login', [LoginController::class, 'showLoginForm'])->name('admin.login');
 Route::post('login', [LoginController::class, 'login'])->name('admin.login.post');
-Route::get('logout', [LoginController::class, 'logout'])->name('admin.logout');
+Route::post('logout', [LoginController::class, 'logout'])->name('admin.logout');
 
 Route::group(['middleware' => ['auth:admin']], function () {
 
