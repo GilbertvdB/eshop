ca5b6d731bf3d86310768520fdb62683da57cd39
gb <gvanderbiezen@gmail.com>
2023-03-01 22:12:09 +0100

    setting up files for Setup
    created settings model, migration, seeder & SettingServiceProvider. Added the last one to the config app

---
+++ app/Models/Setting.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Config;
+
+class Setting extends Model
+{
+    use HasFactory;
+
+
+    /**
+     * @var string
+     */
+    protected $table = 'settings';
+
+    /**
+     * @var array
+     */
+    protected $fillable = ['key', 'value'];
+
+    /**
+     * @param $key
+     */
+    public static function get($key)
+    {
+        $setting = new self();
+        $entry = $setting->where('key', $key)->first();
+        if (!$entry) {
+            return;
+        }
+        return $entry->value;
+    }
+
+    /**
+     * @param $key
+     * @param null $value
+     * @return bool
+     */
+    public static function set($key, $value = null)
+    {
+        $setting = new self();
+        $entry = $setting->where('key', $key)->firstOrFail();
+        $entry->value = $value;
+        $entry->saveOrFail();
+        config(['settings.' . $key => $value]);
+        if (config('settings.' . $key) === $value) {
+            return true;
+        }
+        return false;
+    }
+}
