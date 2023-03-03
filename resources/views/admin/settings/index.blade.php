158cc9646f4def859173a692ee7a06f2cf5fadda
gb <gvanderbiezen@gmail.com>
2023-03-03 23:08:20 +0100

    styled the settings tab
    added a script to display active button with a colored left border

--- resources/views/admin/settings/index.blade.php
+++ resources/views/admin/settings/index.blade.php
@@ -10,74 +10,77 @@
             <div class="bg-gray-200 overflow-hidden shadow-sm sm:rounded-lg">
 
             <div class="flex">
-  <div class="border border-gray-300 bg-gray-100 w-1/5 h-86">
-    <button class="block bg-transparent text-black py-5 px-4 w-full text-left focus:outline-none hover:bg-gray-200 active:bg-gray-300" onclick="openSettings(event, 'General')" id="defaultOpen">General</button>
-    <button class="block bg-transparent text-black py-5 px-4 w-full text-left focus:outline-none hover:bg-gray-200 active:bg-gray-300" onclick="openSettings(event, 'Logo')">Site Logo</button>
-    <button class="block bg-transparent text-black py-5 px-4 w-full text-left focus:outline-none hover:bg-gray-200 active:bg-gray-300" onclick="openSettings(event, 'Footer-seo')">Footer &amp; SEO</button>
-    <button class="block bg-transparent text-black py-5 px-4 w-full text-left focus:outline-none hover:bg-gray-200 active:bg-gray-300" onclick="openSettings(event, 'Social-links')">Social Links</button>
-    <button class="block bg-transparent text-black py-5 px-4 w-full text-left focus:outline-none hover:bg-gray-200 active:bg-gray-300" onclick="openSettings(event, 'Analytics')">Analytics</button>
-    <button class="block bg-transparent text-black py-5 px-4 w-full text-left focus:outline-none hover:bg-gray-200 active:bg-gray-300" onclick="openSettings(event, 'Payments')">Payments</button>
-  </div>
+                <div class="border border-gray-300 bg-white w-1/5 h-86">
+                    <button class="tablinks block text-black py-3 px-4 w-full text-left focus:outline-none hover:bg-gray-200" onclick="openSettings(event, 'General')" >General</button>
+                    <button class="tablinks block text-black py-3 px-4 w-full text-left focus:outline-none hover:bg-gray-200" onclick="openSettings(event, 'Logo')">Site Logo</button>
+                    <button class="tablinks block text-black py-3 px-4 w-full text-left focus:outline-none hover:bg-gray-200" onclick="openSettings(event, 'Footer-seo')">Footer &amp; SEO</button>
+                    <button class="tablinks block text-black py-3 px-4 w-full text-left focus:outline-none hover:bg-gray-200" onclick="openSettings(event, 'Social-links')">Social Links</button>
+                    <button class="tablinks block text-black py-3 px-4 w-full text-left focus:outline-none hover:bg-gray-200" onclick="openSettings(event, 'Analytics')">Analytics</button>
+                    <button class="tablinks block text-black py-3 px-4 w-full text-left focus:outline-none hover:bg-gray-200" onclick="openSettings(event, 'Payments')">Payments</button>
+                </div>
 
-  <div id="General" class="tabcontent border border-gray-300 w-4/5 h-72 hidden">
-    <h3 class="text-2xl font-bold">General</h3>
-      @include('admin.settings.includes.general')
-  </div>
+                <div id="General" class="tabcontent border bg-white border-gray-300 w-4/5 h-72 ml-4 p-2 hidden">
+                    <h3 class="text-2xl font-bold">General</h3>
+                    @include('admin.settings.includes.general')
+                </div>
 
-  <div id="Logo" class="tabcontent border border-gray-300 w-4/5 h-72 hidden">
-    <h3 class="text-2xl font-bold">Site Logo</h3>
-      @include('admin.settings.includes.logo')
-  </div>
+                <div id="Logo" class="tabcontent border bg-white border-gray-300 w-4/5 h-72 ml-4 p-2 hidden">
+                    <h3 class="text-2xl font-bold">Site Logo</h3>
+                    @include('admin.settings.includes.logo')
+                </div>
 
-  <div id="Footer-seo" class="tabcontent border border-gray-300 w-4/5 h-72 hidden">
-    <h3 class="text-2xl font-bold">Footer &amp; SEO</h3>
-      @include('admin.settings.includes.footer_seo')
-  </div>
+                <div id="Footer-seo" class="tabcontent border bg-white border-gray-300 w-4/5 h-72 ml-4 p-2 hidden">
+                    <h3 class="text-2xl font-bold">Footer &amp; SEO</h3>
+                    @include('admin.settings.includes.footer_seo')
+                </div>
 
-  <div id="Social-links" class="tabcontent border border-gray-300 w-4/5 h-72 hidden">
-    <h3 class="text-2xl font-bold">Social Links</h3>
-      @include('admin.settings.includes.social_links')
-  </div>
+                <div id="Social-links" class="tabcontent border bg-white border-gray-300 w-4/5 h-72 ml-4 p-2 hidden">
+                    <h3 class="text-2xl font-bold">Social Links</h3>
+                    @include('admin.settings.includes.social_links')
+                </div>
 
-  <div id="Analytics" class="tabcontent border border-gray-300 w-4/5 h-72 hidden">
-    <h3 class="text-2xl font-bold">Analytics</h3>
-      @include('admin.settings.includes.analytics')
-  </div>
+                <div id="Analytics" class="tabcontent border bg-white border-gray-300 w-4/5 h-72 ml-4 p-2 hidden">
+                    <h3 class="text-2xl font-bold">Analytics</h3>
+                    @include('admin.settings.includes.analytics')
+                </div>
 
-  <div id="Payments" class="tabcontent border border-gray-300 w-4/5 h-72 hidden">
-    <h3 class="text-2xl font-bold">Payments</h3>
-      @include('admin.settings.includes.payments')
-  </div>
-</div>
+                <div id="Payments" class="tabcontent border bg-white border-gray-300 w-4/5 h-72 ml-4 p-2 hidden">
+                    <h3 class="text-2xl font-bold">Payments</h3>
+                    @include('admin.settings.includes.payments')
+                </div>
+                </div>
 
 
-<script>
+                <script>
 
-function openSettings(evt, settingName) {
-  // Declare all variables
-  var i, tabcontent, tablinks;
+                function openSettings(evt, settingName) {
+                // Declare all variables
+                var i, tabcontent, tablinks;
 
-  // Get all elements with class="tabcontent" and hide them
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
+                // Get all elements with class="tabcontent" and hide them
+                tabcontent = document.getElementsByClassName("tabcontent");
+                for (i = 0; i < tabcontent.length; i++) {
+                    tabcontent[i].style.display = "none";
+                }
 
-  // Get all elements with class="tablinks" and remove the class "active"
-  tablinks = document.getElementsByClassName("tablinks");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
-  }
+                // Get all elements with class="tablinks" and remove the class "active"
+                tablinks = document.getElementsByClassName("tablinks");
+                for (i = 0; i < tablinks.length; i++) {
+                    tablinks[i].className = tablinks[i].className.replace(" active", "");
+                    tablinks[i].className = tablinks[i].className.replace(" border-l-4", "");
+                }
 
-  // Show the current tab, and add an "active" class to the link that opened the tab
-  document.getElementById(settingName).style.display = "block";
-  evt.currentTarget.className += " active";
-} 
+                // Show the current tab, and add an "active" class to the link that opened the tab
+                document.getElementById(settingName).style.display = "block";
+                evt.currentTarget.className += " active";
+                evt.currentTarget.classList.add("border-l-4");
+                evt.currentTarget.classList.add("border-sky-500");
+                } 
 
-// Get the element with id="defaultOpen" and click on it
-document.getElementById("defaultOpen").click();
+                // Get the element with id="defaultOpen" and click on it
+                document.getElementById("defaultOpen").click();
 
-</script>
+                </script>
 
             </div>
         </div>
