6e036426b610b2afe52d1a5f60d24c026fee65e6
gb <gvanderbiezen@gmail.com>
2023-03-13 21:12:05 +0100

    created brands table
    updated brands migration and created table in the data base

--- database/migrations/2023_03_13_175740_create_brands_table.php
+++ database/migrations/2023_03_13_175740_create_brands_table.php
@@ -14,7 +14,10 @@ return new class extends Migration
     public function up()
     {
         Schema::create('brands', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id');
+            $table->string('name', 45);
+            $table->string('slug');
+            $table->string('logo')->nullable();
             $table->timestamps();
         });
     }
