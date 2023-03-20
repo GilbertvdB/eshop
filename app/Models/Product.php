12586f6cfe7b75018dffb7a5c8909f23b3c635b2
gb <gvanderbiezen@gmail.com>
2023-03-21 00:58:52 +0100

    update product migration and model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,14 +3,49 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Relations\Pivot;
+use Illuminate\Support\Str;
 
 class Product extends Pivot
 {
+    /**
+     * @var string
+     */
+    protected $table = 'products';
+
+    /**
+     * @var array
+     */
     protected $fillable = [
-
-        'name', 'price', 'description', 'image'
-
+        'brand_id', 'sku', 'name', 'slug', 'description', 'quantity',
+        'weight', 'price', 'sale_price', 'status', 'featured',
     ];
+
+    /**
+     * @var array
+     */
+    protected $casts = [
+        'quantity'  =>  'integer',
+        'brand_id'  =>  'integer',
+        'status'    =>  'boolean',
+        'featured'  =>  'boolean'
+    ];
+
+     /**
+     * @param $value
+     */
+    public function setNameAttribute($value)
+    {
+        $this->attributes['name'] = $value;
+        $this->attributes['slug'] = Str::slug($value);
+    }
+
+     /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function brand()
+    {
+        return $this->belongsTo(Brand::class);
+    }
 
     public function reviews()
     {
