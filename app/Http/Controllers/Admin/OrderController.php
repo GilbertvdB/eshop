8a9c438f770c874f0b5499f83423eb8da34f66e0
gb <gvanderbiezen@gmail.com>
2023-04-15 23:51:00 +0200

    Update OrderController.php
    update with the function for index and show

--- app/Http/Controllers/Admin/OrderController.php
+++ app/Http/Controllers/Admin/OrderController.php
@@ -15,5 +15,18 @@ class OrderController extends Controller
         $this->orderRepository = $orderRepository;
     }
 
-    
+    public function index()
+    {
+        $orders = $this->orderRepository->listOrders();
+        $this->setPageTitle('Orders', 'List of all orders');
+        return view('admin.orders.index', ['orders' => $orders]);
+    }
+
+    public function show($orderNumber)
+    {
+        $order = $this->orderRepository->findOrderByNumber($orderNumber);
+
+        $this->setPageTitle('Order Details', $orderNumber);
+        return view('admin.orders.show', ['order' => $order]);
+    }
 }
